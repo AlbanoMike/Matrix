f67127fca3d100e1d2f16e2fa225a223b4bb8e6a
perkles <michal.albanodias@gamil.com>
2023-09-25 21:23:46 -0300

    add

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class asdasdas {
+public class Matrix {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Integer a;
